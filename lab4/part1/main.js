081c7b6de115ce249c661b35842c9a15bd9b1738
Beatlemaniac-JoJo <joel.gabud@dcmail.ca>
2025-03-17 14:10:32 -0400

    Update main.js

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -7,18 +7,47 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-variable storyText() {
-  alert("It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.");
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+let insertX = [
+	"Willy the Goblin",
+	"Big Daddy",
+	"Father Christmas"
+];
+
+let insertY = [
+	"the soup kitchen",
+	"Disneyland",
+	"the White House"
+];
+
+let insertZ = [
+	"spontaneously combusted",
+	"melted into a puddle on the sidewalk",
+	"turned into a slug and crawled away"
+];
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
 }
 
-Willy the Goblin
-Big Daddy
-Father Christmas
+let newStory = storyText;
 
-the soup kitchen
-Disneyland
-the White House
-
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
+xItem = randomValueFromArray(insertX);
+yItem = randomValueFromArray(insertY);
+zItem = randomValueFromArray(insertZ);
