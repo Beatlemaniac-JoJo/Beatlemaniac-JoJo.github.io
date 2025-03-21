a72e37d04141bbf70089617345e4b75ffa94d71e
Beatlemaniac-JoJo <joel.gabud@dcmail.ca>
2025-03-21 14:57:32 -0400

    Update main.js

--- lab4/part2/js/main.js
+++ lab4/part2/js/main.js
@@ -5,14 +5,29 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const imageNames = [
+      "images/pic1.jpg",
+      "images/pic2.jpg",
+      "images/pic3.jpg",
+      "images/pic4.jpg",
+      "images/pic5.jpg"
+];
+      
 /* Declaring the alternative text for each image file */
+const imageAlternatives = [
+      "Closeup on an eye",
+      "Many swirling patterns",
+      "Various purple flowers",
+      "A picture of hieroglyphics",
+      "A butterfly on a leaf"
+];
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
+for(let i = 0; i < imageNames.length; i++)
+{
+	const newImage = document.createElement('img');
+	newImage.setAttribute('src', imageNames[i]);
+	newImage.setAttribute('alt', imageAlternatives[i]);
+	thumbBar.appendChild(newImage);
+}
 /* Wiring up the Darken/Lighten button */
