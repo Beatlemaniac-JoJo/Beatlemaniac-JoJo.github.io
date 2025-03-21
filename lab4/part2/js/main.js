0f53802572e885a76fd5c336bd759b9ca3e731f0
Beatlemaniac-JoJo <joel.gabud@dcmail.ca>
2025-03-21 15:08:17 -0400

    Update main.js

--- lab4/part2/js/main.js
+++ lab4/part2/js/main.js
@@ -31,3 +31,17 @@ for(let i = 0; i < imageNames.length; i++)
 	thumbBar.appendChild(newImage);
 }
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    const currentClass = btn.getAttribute('class');
+
+    if (currentClass === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)'; // Darken
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)'; // Lighten
+    }
+});
+
